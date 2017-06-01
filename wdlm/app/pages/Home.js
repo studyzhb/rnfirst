411e7ddfd2cb7c5adb8b29be4115d4c87b2ef3b7
zhb-company <445798850@qq.com>
2017-06-01 18:22:20 +0800

    登录页完成

---
+++ wdlm/app/pages/Home.js
@@ -0,0 +1,17 @@
+'use strict';
+import React,{Component} from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+export default class Home extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <Text >Home</Text>
+        )
+    }
+}
---
+++ wdlm/app/pages/Order.js
@@ -0,0 +1,17 @@
+'use strict';
+import React,{Component} from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+export default class Order extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <Text >Order</Text>
+        )
+    }
+}
